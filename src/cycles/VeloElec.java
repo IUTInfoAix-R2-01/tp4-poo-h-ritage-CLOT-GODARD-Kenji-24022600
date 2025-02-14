0067df6b49e0241d06734b9ffa9a7d150eb568a9
Clot-Godard Kenji <kenji.clot-godard@etu.univ-amu.fr>
2025-02-14 11:34:15 +0100

    Ajout de la question 5 à 8

--- src/cycles/VeloElec.java
+++ src/cycles/VeloElec.java
@@ -36,10 +36,12 @@ public class VeloElec extends Velo{
 		this.facteurPuissanceMoteur = coupleMoteur;
 	}
 	
+	@Override
 	public String toString() {
         return "VeloElec [facteurPuissanceMoteur=" + facteurPuissanceMoteur + "]";
     }
 	
+	@Override
 	public double getPuissance(double FrequenceCoupDePedale) {
         return super.getPuissance(FrequenceCoupDePedale) * facteurPuissanceMoteur;
     }
